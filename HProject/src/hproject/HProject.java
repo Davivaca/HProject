f5d09f96c5101ca0dbc0834484860bd092bfa616
Davi vaccarezza <76525758+Stirkexd@users.noreply.github.com>
2022-11-25 23:29:08 -0300

    Comentarios
    Apenas algumas adições de comentários para deixar o código mais didático

--- HProject/src/hproject/HProject.java
+++ HProject/src/hproject/HProject.java
@@ -14,32 +14,30 @@ public class HProject {
         InterfacePrimit inter1;//aqui criamos um objeto da classe interface, que possui todos os metodos que serao usados para interagir com o usuario
         inter1 = new InterfacePrimit();
         cont11.apresentar();
-        Scanner leruser = new Scanner(System.in);
-        Scanner lersenha = new Scanner(System.in);
+        Scanner leruser = new Scanner(System.in);//um scanner para ler o usuario
+        Scanner lersenha = new Scanner(System.in);//um scanner para ler a senha
         String senha;
         String user;
-        user = leruser.next();
-        senha = lersenha.next();
+        user = leruser.next();//leitura do usuario
+        senha = lersenha.next();//leitura da senha
         if(cont11.verificacao(user,senha)==true&&cont11.senhacorreta(user, senha)==true){//verifica se o usuario e senha estao corretos
         cont11.identificacao(user);//chama o metodo para identificar o nome de usuario
         if(cont11.verificacao(user,senha)==true){
             cont11.identificacao(user);//chama o metodo para identificar o nome de usuario
-            switch (cont11.getVerifi()) {
+            switch (cont11.getVerifi()) {//pega o resultado da verificacao do usuario para usar nos cases
                 case 1:
-                System.out.println("ADM");
-                inter1.interadm();
+                inter1.interadm();//inicia o sitema do administrador
                     break;
                 case 2:
-                    inter1.interrec();
+                    inter1.interrec();//inicia o sitema da recepcao
                     break;
                 case 3:
-                    System.out.println("MED");
-                    inter1.intermed();
+                    inter1.intermed();//inicia o sitema do medico
                     break;
                 default:
                     break;
             }
-        }else{
+        }else{//caso o cadastro seja invalidado
         System.out.println("------------------");
         System.out.println("Cadastro invalido!");
         }
